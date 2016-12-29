7149bec08d6627fa781e742aba8f79fedb102074
Kristina Ogurova <okv2886@gmail.com>
2016-12-29 19:42:57 +0200

    add homework2

--- JavaScript/Homework2/js/script.js
+++ JavaScript/Homework2/js/script.js
@@ -8,9 +8,17 @@ for (var i = 0; i < 5; i++) {
 }
 
 var userName = prompt("Введите имя пользователя", "");
+var in_arr = false;
 
 for (var i = 0; i < arrName.length-1; i++) {
-   if (arrName[i] == userName) {
-     alert(userName + ', вы успешно вошли.');
-   }
+
+  if (arrName[i] == userName) {
+    in_arr = true;
+    alert(userName + ', вы успешно вошли.');
+  }
+
+}
+
+if  (in_arr == false) {
+  alert('Неверно введено имя пользователя');
 }
